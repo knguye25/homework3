da945ddaf0be91ee03d0508c69f785af6b13e077
Khanh Nguyen <knguye22@gmail.com>
2016-03-02 22:31:41 -0500

    basic player movement

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	/* Public variables*/
+	public float speed;
+	public bool isGrounded, facingRight;
+	public LayerMask groundLayer;
+
+	/* Private variables*/
+	private Rigidbody2D playerBody;
+	private Collider2D playerCollider;
+
+	void Start () {
+		playerBody = GetComponent<Rigidbody2D> ();
+		playerCollider = GetComponent<Collider2D> ();
+		facingRight = true;
+		isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		//Debug.Log ("testing key input" + horizontal);
+		Movement(horizontal);
+		Flip (horizontal);
+	}
+
+	void Update(){
+		isGrounded = Physics2D.IsTouchingLayers (playerCollider, groundLayer);
+		if (Input.GetKey (KeyCode.Space) && isGrounded)
+			playerBody.velocity = new Vector2 (playerBody.velocity.x, speed);
+	}
+
+	private void Movement(float horizontal){
+		playerBody.velocity = new Vector2 (horizontal * speed, playerBody.velocity.y);
+	}
+
+	private void Flip(float horizontal){
+		if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight)) {
+			facingRight = !facingRight;
+			Vector3 theFlipScale = transform.localScale;
+			theFlipScale.x *= -1;
+			transform.localScale = theFlipScale;
+		}
+	}
+}
