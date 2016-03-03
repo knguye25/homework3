6fb67a9028547c139de3feb3027c8f261fff852e
Khanh Nguyen <knguye22@gmail.com>
2016-03-03 02:40:50 -0500

    added menus and lose and win screens

---
+++ Assets/PlayAgain.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayAgain : MonoBehaviour {
+	public void restartLvl(){
+		Debug.Log("restarting");
+		SceneManager.LoadScene ("GameScene");
+	}
+
+	public void quitLvl(){
+		Debug.Log ("quit");
+		Application.Quit ();
+	}
+
+	public void helpLvl(){
+		Debug.Log ("help");
+		SceneManager.LoadScene ("HelpScene");
+	}
+
+	public void titleLvl(){
+		Debug.Log ("Back to Title");
+		SceneManager.LoadScene ("TitleScene");
+	}
+}
---
+++ Assets/finishFlagScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Collections;
+
+public class finishFlagScript : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			Debug.Log("Done");
+		}
+	}
+}
