fbb83dd0175541838b78e6178c987bb95ce389bb
Khanh Nguyen <knguye22@gmail.com>
2016-03-02 23:21:34 -0500

    finish touch on animation

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -11,10 +11,12 @@ public class PlayerScript : MonoBehaviour {
 	/* Private variables*/
 	private Rigidbody2D playerBody;
 	private Collider2D playerCollider;
+	private Animator playerAnimator;
 
 	void Start () {
 		playerBody = GetComponent<Rigidbody2D> ();
 		playerCollider = GetComponent<Collider2D> ();
+		playerAnimator = GetComponent<Animator> ();
 		facingRight = true;
 		isGrounded = true;
 	}
@@ -22,8 +24,10 @@ public class PlayerScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		float horizontal = Input.GetAxis ("Horizontal");
-		//Debug.Log ("testing key input" + horizontal);
-		Movement(horizontal);
+		Movement(horizontal,speed);
+		if(Input.GetKey(KeyCode.LeftShift))
+			Movement(horizontal,speed+10);
+
 		Flip (horizontal);
 	}
 
@@ -31,10 +35,13 @@ public class PlayerScript : MonoBehaviour {
 		isGrounded = Physics2D.IsTouchingLayers (playerCollider, groundLayer);
 		if (Input.GetKey (KeyCode.Space) && isGrounded)
 			playerBody.velocity = new Vector2 (playerBody.velocity.x, speed);
+		playerAnimator.SetBool ("isGrounded", isGrounded);
 	}
 
-	private void Movement(float horizontal){
-		playerBody.velocity = new Vector2 (horizontal * speed, playerBody.velocity.y);
+	private void Movement(float horizontal, float speedVelocity){
+		playerBody.velocity = new Vector2 (horizontal * speedVelocity, playerBody.velocity.y);
+		playerAnimator.SetFloat ("Speed", Mathf.Abs (horizontal * speedVelocity));
+		Debug.Log ("speed = " + (horizontal * speed));
 	}
 
 	private void Flip(float horizontal){
